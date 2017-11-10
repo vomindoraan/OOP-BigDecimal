adf1d73859f58b9ccf97aa2eab13d7cb3e8baaba
Konstantin Đorđević <vomindoraan@gmail.com>
2017-11-10 15:06:19 +0100

    Print test cases as Python statements

--- Test/Main.cpp
+++ Test/Main.cpp
@@ -11,39 +11,28 @@ void test();
 	"0", "0.", "+0.0", ".0", "163.445", "+000163.44500", \
 	"0.5", ".5", "4", "-0.", "30", "+18.11131", "-41.5", \
 }
-#define SEP "    "
 
 void test()
 {
+	std::cout << "from __future__ import print_function\n\n";
+
 	const char* raw[] = VALUES;
 	BigDecimal  arr[] = VALUES;
 	constexpr auto n = sizeof arr / sizeof *arr;
 
-	for (const auto& bd : arr)
-		std::cout << bd << " ";
-	std::cout << "\n";
-
 	for (size_t i = 0; i < n; ++i)
 		for (size_t j = 0; j < n; ++j) {
-			std::cout << "\n" << raw[i] << ", " << raw[j] << "\n";
 			auto& a = arr[i];
 			auto& b = arr[j];
-			std::cout << "a+b: " << (a + b)  << SEP;
-			std::cout << "a-b: " << (a - b)  << SEP;
-			std::cout << "a>b: " << (a > b)  << SEP;
-			std::cout << "a<b: " << (a < b)  << SEP;
-			std::cout << "a=b: " << (a == b) << SEP;
-			std::cout << "a®4: " << (a << 4) << ", b®4: " << (b << 4) << SEP;
-			std::cout << "a¯4: " << (a >> 4) << ", b¯4: " << (b >> 4) << SEP;
-			std::cout << "~a: "  << ~a << ", ~b: "  << ~b << SEP;
-			std::cout << "|a|: " << +a << ", |b|: " << +b << SEP;
-			std::cout << "-a: "  << -a << ", -b: "  << -b << "\n";
+			std::cout << "print((" << a << " + "  << b << ") == " << (a + b)  << ")\n";
+			std::cout << "print((" << a << " - "  << b << ") == " << (a - b)  << ")\n";
+			std::cout << "print((" << a << " < "  << b << ") == " << (a < b)  << ")\n";
+			std::cout << "print((" << a << " > "  << b << ") == " << (a > b)  << ")\n";
+			std::cout << "print((" << a << " == " << b << ") == " << (a == b) << ")\n";
+			std::cout << "print((" << a << " != " << b << ") == " << (a != b) << ")\n";
+			std::cout << "print((" << a << " <= " << b << ") == " << (a <= b) << ")\n";
+			std::cout << "print((" << a << " >= " << b << ") == " << (a >= b) << ")\n";
 		}
-
-	BigDecimal zero;
-	BigDecimal fortyTwo = 42;
-	std::cout << zero << " " << fortyTwo << " " << (zero <= fortyTwo) << "\n";
-	std::cout << (zero == 0) << " " << (fortyTwo >= 0) << "\n";
 }
 #endif  // STUDENT_TEST
 
@@ -54,5 +43,5 @@ int main()
 	test();
 
 	auto ms = (std::clock() - start) * 1000.0 / CLOCKS_PER_SEC;
-	std::cout << "\nTrajanje: " << ms << " ms\n";
+	std::cout << "\nprint('Trajanje: ', " << ms << ", ' ms')\n";
 }
